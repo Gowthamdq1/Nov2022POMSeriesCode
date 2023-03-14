00741635763fed95b385b7bdc27ed7c6d1ee71f8
Gowtham Sivaji <gowtham.sivaji@ivygdcad.ivy-support.com>
2023-03-14 09:16:03 +0530

    Updated order page as per gowtham comments

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -5,9 +5,13 @@ import org.openqa.selenium.By;
 public class OrderPage {
 	
 	By loc = By.id("order");
+	By price = By.id("price");
 	
 	public void getOrder() {
 		System.out.println("Get Order");
+	}
+	public void getPrice() {
+		System.out.println("Get Price");
 	}
 
 }
