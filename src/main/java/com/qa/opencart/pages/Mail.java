37d6fdfd0150135bdfe86bd1c67286df7b13daae
Gowtham Sivaji <gowtham.sivaji@ivygdcad.ivy-support.com>
2023-03-14 20:10:47 +0530

    Mail Gowtham

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class Mail {
+  
+  public void sendMail(){
+    System.out.println("Mail Sent Gowtham");
+  }
+  public void getMail() {
+	  System.out.println("Get Mail Gowtham");
+  }
+	
+
+}
